2330dffa6494bee0af691b2d54b32fa8310c51ea
Borislav <bobipipev@hotmail.com>
2016-04-18 14:43:24 +0000

    handle js ddos test

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,26 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var cloudscraper = require('cloudscraper');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Website' });
 });
 
 router.post('/', function (req, res) {
-  website = req.body.title;
-  website_data = request.get(website).on('error', function(err) {
-      console.log(err)
-  }).pipe(res);
+  var website = req.body.title;
+  // request.get(website).on('error', function(err) {
+  //     console.log(err);
+  // }).pipe(res);
+  cloudscraper.get(website, function(error, response, body) {
+    if (error) {
+      console.log('Error occurred');
+    } else {
+      res.send(body);
+    }
+  });
+  
 });
 
 module.exports = router;
